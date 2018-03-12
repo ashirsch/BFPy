Fixed wavelength basis parameter mutation by storing original wavelength and wavelength count alongside one used and changed in calculation.

bfpy/vis/visualization.py
@@ -4,13 +4,14 @@ import matplotlib.pyplot as plt
 
 def basis_func_plot(basis, wavelength_ind, crop=False):
     plot_col = basis.basis_matrix[:,wavelength_ind].todense()
-    if crop: # TODO: get this working with MD / EQ basis
+    if crop: # TODO: get this working with MD / EQ basis and shorter wavelength ranges
+        wavelength_ind = wavelength_ind % basis.basis_parameters.orig_wavelength_count
         begin_ind = int(wavelength_ind-np.floor(basis.basis_parameters.ux_count/2))*basis.basis_parameters.uy_count
         end_ind = begin_ind + basis.basis_parameters.uy_count*basis.basis_parameters.ux_count
         plot_col = plot_col[begin_ind:end_ind]
         plot_width = basis.basis_parameters.ux_count
-    elif basis.basis_parameters.wavelength_count > basis.basis_parameters.ux_count:
-        plot_width = basis.basis_parameters.wavelength_count
+    elif basis.basis_parameters.orig_wavelength_count > basis.basis_parameters.ux_count:
+        plot_width = basis.basis_parameters.orig_wavelength_count
     else:
         plot_width = basis.basis_parameters.ux_count
     plot_col = np.reshape(plot_col, (basis.basis_parameters.uy_count, plot_width), order='F')

main.py
@@ -9,14 +9,14 @@ from bfpy.vis import visualization as bfpvis
 print("starting numba with {0} threads".format(numba.config.NUMBA_NUM_THREADS))
 
 h_bp = BasisParameters(basis_type="EDIso",
-                     n0=1.0, n1=1.0, n2o=1.7, n2e=1.7, n3=1.5,
-                     ux_range=(-1.3,1.3), uy_range=(-1.3,1.3),
-                     ux_count=180, uy_count=180,
-                     d=10.0, s=10.0, l=0.0,
-                     wavelength=np.linspace(554.7395, 684.6601, 1024, dtype=np.float64),
-                     wavelength_count=1024,
-                     pol_angle=0,
-                     pad_w=False)
+                       n0=1.0, n1=1.0, n2o=1.7, n2e=1.7, n3=1.5,
+                       ux_range=(-1.3,1.3), uy_range=(-1.3,1.3),
+                       ux_count=180, uy_count=180,
+                       d=10.0, s=10.0, l=0.0,
+                       wavelength=np.linspace(554.7395, 684.6601, 1024, dtype=np.float64),
+                       wavelength_count=1024,
+                       pol_angle=0,
+                       pad_w=False, trim_w=True)
 v_bp = BasisParameters(basis_type="EDIso",
                        n0=1.0, n1=1.0, n2o=1.7, n2e=1.7, n3=1.5,
                        ux_range=(-1.3, 1.3), uy_range=(-1.3, 1.3),
@@ -25,7 +25,7 @@ v_bp = BasisParameters(basis_type="EDIso",
                        wavelength=np.linspace(554.7395, 684.6601, 1024, dtype=np.float64),
                        wavelength_count=1024,
                        pol_angle=np.pi/2,
-                       pad_w=True, trim_w=True)
+                       pad_w=False, trim_w=True)
 
 t0 = time.time()
 # ========= TEST CODE HERE =========
@@ -39,6 +39,7 @@ v_basis.build()
 t1 = time.time()
 print("Done with elapsed time: {0:.3f} sec".format(t1-t0))
 
-# bfpvis.basis_func_plot(v_basis, 0, crop=False)
-# bfpvis.basis_func_plot(v_basis, 0, crop=False)
-# bfpvis.basis_func_plot(v_basis, 1, crop=False)
+bfpvis.basis_func_plot(h_basis, 400)
+bfpvis.basis_func_plot(h_basis, 1424)
+bfpvis.basis_func_plot(v_basis, 400)
+bfpvis.basis_func_plot(v_basis, 1424)