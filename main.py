392e4f74aef2377395e5f2dea67bbc44914a0883
ashirsch <alexander_hirsch@brown.edu>
2018-04-10 02:34:01 -0400

    Updated main test to reflect new API scheme.

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ import bfpy.vis.visualization as bfpvis
 
 print("starting numba with {0} threads".format(numba.config.NUMBA_NUM_THREADS))
 
-h_basis = bfpy.basis.IsometricEmitter(pol_angle=90, dipoles=("ED", "MD"))
-h_basis.define(n2=1.7, n3=1.5)
+h_basis = bfpy.basis.IsometricEmitter(pol_angle=90, dipoles=("ED", "MD"), n2=1.7, n3=1.5)
 h_basis.define_observation_parameters(np.linspace(554, 660, 1024, dtype=np.float64), 180)
+
+or_basis = bfpy.basis.OrientedEmitter(pol_angle=0, n2e=1.7, n2o=1.6, n3=1.5, NA=1.5)
+or_basis.define_observation_parameters(np.linspace(660, 740, 1024, dtype=np.float64), 180)
 
 t0 = time.time()
 # ========= TEST CODE HERE =========
 h_basis.build()
+or_basis.build()
 # ==================================
 t1 = time.time()
 print("Done with elapsed time: {0:.3f} sec".format(t1-t0))
 
 bfpvis.basis_func_plot(h_basis, 400)
 bfpvis.basis_func_plot(h_basis, 1424)
-# bfpvis.basis_func_plot(v_basis, 400)
-# bfpvis.basis_func_plot(v_basis, 1424)
+bfpvis.basis_func_plot(or_basis, 400)
+bfpvis.basis_func_plot(or_basis, 1424)
